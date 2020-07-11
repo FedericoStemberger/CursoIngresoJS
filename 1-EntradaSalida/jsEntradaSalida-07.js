be1d46f35846ef4f8d9b33ab52a5f87bfaa0affc
FedericoStemberger <fedestemberger@gmail.com>
2020-07-11 13:16:51 -0300

    Ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -3,23 +3,55 @@ Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+	var numeroingresado1;
+	var numeroingresado2;
+	var resultado;
+
 function sumar()
 {	
-	alert("ok");	
+	numeroingresado1=txtIdNumeroUno.value
+	numeroingresado2=txtIdNumeroDos.value
+
+	numeroingresado1 = parseInt(numeroingresado1)
+	numeroingresado2 = parseInt(numeroingresado2)
+	resultado =numeroingresado1+numeroingresado2
+
+	alert("El resultado de la suma es: "+resultado);	
 }
 
 function restar()
 {
-	
+	numeroingresado1=txtIdNumeroUno.value
+	numeroingresado2=txtIdNumeroDos.value
+
+	numeroingresado1 = parseInt(numeroingresado1)
+	numeroingresado2 = parseInt(numeroingresado2)
+	resultado =numeroingresado1-numeroingresado2
+
+	alert("El resultado de la resta es: "+resultado);
 }
 
 function multiplicar()
 { 
-	
+	numeroingresado1=txtIdNumeroUno.value
+	numeroingresado2=txtIdNumeroDos.value
+
+	numeroingresado1 = parseInt(numeroingresado1)
+	numeroingresado2 = parseInt(numeroingresado2)
+	resultado =numeroingresado1*numeroingresado2
+
+	alert("El resultado de la multiplicación es: "+resultado);
 }
 
 function dividir()
 {
-	
+	numeroingresado1=txtIdNumeroUno.value
+	numeroingresado2=txtIdNumeroDos.value
+
+	numeroingresado1 = parseInt(numeroingresado1)
+	numeroingresado2 = parseInt(numeroingresado2)
+	resultado =numeroingresado1/numeroingresado2
+
+	alert("El resultado de la división es: "+resultado);
 }
 
