75ca5a630fdb54a9281de44cbaa93f9610caf788
FedericoStemberger <fedestemberger@gmail.com>
2020-07-10 23:32:44 -0300

    Ejercicio 5

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -3,6 +3,12 @@ Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	alert("ok");
+	var nombreIngresado1;
+	var nombreIngresado2;
+
+	nombreIngresado1=txtIdNombre.value
+	nombreIngresado2=txtIdEdad.value
+
+	alert(nombreIngresado1 + nombreIngresado2);
 }
 
