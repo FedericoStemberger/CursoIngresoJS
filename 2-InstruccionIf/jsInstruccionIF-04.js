a425dc4e202ce18ea548a3cf09cd619af7b827b3
FedericoStemberger <fedestemberger@gmail.com>
2020-07-12 13:16:58 -0300

    Ejercicio 4

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,14 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+	var edad;
+	var adolescente
+
+	edad = txtIdEdad.value
+
+	if
+	(edad>=13 && edad<=17)
+	alert("La persona es adolescente");
 
 
 }//FIN DE LA FUNCIÓN
