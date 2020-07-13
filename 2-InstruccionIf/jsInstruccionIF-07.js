01d5d79689cf30bffd7413eecd45e05cce3d3075
FedericoStemberger <fedestemberger@gmail.com>
2020-07-13 20:05:54 -0300

    Ejercicios

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,13 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	//Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", mostrar el siguiente mensaje: 'Es muy pequeño para NO ser soltero.'
+	var edad;
+	var estado;
 
+	edad = txtIdEdad.value;
+	estado = estadoCivil.value;
+
+	if (edad<18 && estado != "Soltero")
+	alert("Es muy pequeño para NO ser soltero.");
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,7 +1,15 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	/*Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", NO HACER NADA,
+pero si no es asi, y es soltero y no es menor, mostrar el siguiente mensaje: 'Es soltero y no es menor.'*/
+	var edad;
+	var estado;
+
+	edad = txtIdEdad.value;
+	estado = estadoCivil.value
+
+	if (edad>=18 && estado == "Soltero")
+	alert("Es soltero y no es menor.");
 	
 
 
