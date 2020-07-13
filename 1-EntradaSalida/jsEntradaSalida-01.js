c5137eb06c82bcf4fde5abc22aaf118b2829ee48
FedericoStemberger <fedestemberger@gmail.com>
2020-07-13 10:22:56 -0300

    Ejercicio 1

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,6 +1,5 @@
 /*
-Al presionar el  botón, 
-se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+Al presionar el botón, se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla".*/
 function mostrar()
 {
 	alert("Esto funciona de maravilla");
