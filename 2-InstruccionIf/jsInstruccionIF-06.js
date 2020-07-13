8486223256098380bd6098019328913c1549c8f4
FedericoStemberger <fedestemberger@gmail.com>
2020-07-13 15:20:00 -0300

    Ejercicios

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,7 +1,18 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//Al ingresar una edad debemos informar si la persona es mayor de edad (mas de 18 años) o adolescente (entre 13 y 17 años) o niño (menor a 13 años).
+	var edad;
+
+	edad = txtIdEdad.value
+
+	if (edad>=18)
+	alert("La persona es mayor de edad");
+	
+	else if(edad>=13 && edad<=17)
+	alert("La persona es adolescente");
+	
+	else 
+	alert("La persona es niña");
 
 
 
